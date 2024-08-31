4b5212dd1ba0f184de427f8fea252f8aff330b43
angelrojo209 <159593429+angelrojo209@users.noreply.github.com>
2024-08-30 18:48:31 -0700

    Add files via upload

---
+++ basededatosweb/parcial 1/Parcial 1/Holamundo.php
@@ -0,0 +1,14 @@
+<?php
+
+$nombre = "Angel";
+$numero = 4;
+$numero2 = "4";
+$total = $numero + $numero2;
+echo "total =" . total;
+echo "<h1> HOLA MUNDO </h1>";
+
+for($i=0; $i<10; $i++){
+    echo $i . "<br>";
+}
+echo "<h3 style ='color: red ;'>Angel saul </h3>";
+?>
