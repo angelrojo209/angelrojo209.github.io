cc238f3fe0d99591c52839f14f406e5aa48b35de
angelrojo209 <159593429+angelrojo209@users.noreply.github.com>
2024-09-27 21:10:42 -0700

    Add files via upload

---
+++ 5to semestre/Parcial 1/practica1.js
@@ -0,0 +1,21 @@
+console.log("Funciona JS");
+
+var cambio = false;
+
+function cambia_color() {
+  if (cambio) {
+    document.getElementById("cuadro").style.backgroundColor = "white";
+    cambio = false;
+  } else {
+    document.getElementById("cuadro").style.backgroundColor = "Yellow";
+    cambio = true;
+  }
+}
+
+function ocultar() {
+  document.getElementById("titulo").style.display = "none";
+}
+
+function mostrar() {
+  document.getElementById("titulo").style.display = "";
+}
